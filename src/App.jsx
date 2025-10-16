a581b65062f5858a52337cb3e02c8fc865fb4b1b
MartinRamirez <maferaro2004@hotmail.com>
2025-10-16 17:43:51 -0500

    CargaArchivosBase

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Busqueda from "./componentes/Busqueda/Busqueda";
+
+function App() {
+  return (
+    <div className="App">
+      <Busqueda />
+    </div>
+  );
+}
+
+export default App;
+
+// Instalaciones hechas
+// Instala Framer Motion y Lucide React para las animaciones e íconos:
+// npm install framer-motion lucide-react
+
+// Instalación de Tailwind CSS para estilos:
+// npm install -D tailwindcss postcss autoprefixer
+// npx tailwindcss init -p
